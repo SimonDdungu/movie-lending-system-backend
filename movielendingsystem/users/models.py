3fd8fe80a955cec6dc39aa89e52f59db4685c8e9
SimonDdungu <dev.simonddungu@gmail.com>
2025-09-17 12:36:02 +0300

    Minor Update

--- movielendingsystem/users/models.py
+++ movielendingsystem/users/models.py
@@ -3,8 +3,7 @@ from django.db import models
 
 class Users(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    firstname = models.CharField(max_length=100)
-    lastname = models.CharField(max_length=100)
+    name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phonenumber = models.CharField(max_length=15, unique=True)
     NIN = models.CharField(max_length=20, unique=True)  
